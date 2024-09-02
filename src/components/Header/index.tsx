508460f06f28e34221164fa55ec53611f32a2171
Sajermann <sajermannbruno@gmail.com>
2024-09-02 17:00:43 -0300

    fix header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,7 +5,7 @@ export function Header() {
   return (
     <header className="w-full h-20 bg-zinc-950 text-white flex items-center justify-center px-4">
       <span className="flex-1 text-center text-4xl font-extrabold">
-        My TODOs By Sajermann
+        My TODOs
       </span>
       <div className="flex gap-4">
         <ToggleDarkMode />
